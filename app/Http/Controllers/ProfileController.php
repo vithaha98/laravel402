15699375da29a3c87c3831645f40d6aacfbea576
= <=>
2020-01-11 18:40:07 +0700

    laravel

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Http\Controllers;
+use Illuminate\Routing\Controller as BaseController;
+
+
+class ProfileController extends BaseController
+{
+    public function showProfile($name){
+        return view('profile')->with('name',$name);
+    }
+}
---
+++ database/migrations/2020_01_08_190444_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->text('content');
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
